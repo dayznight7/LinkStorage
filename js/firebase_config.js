c16e50d4f32ed45a5bf547e168a07892a6cae1b0
dayznight7 <dayznight7@gmail.com>
2022-04-24 17:18:02 +0900

    first commit

---
+++ js/firebase_config.js
@@ -0,0 +1,23 @@
+/*
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-database.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-functions.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-storage.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-analytics.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-remote-config.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-performance.js"></script> 
+*/
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD5w7MSZrohow-nd_UeoGZ1bctLI09XNfM",
+  authDomain: "linkstorage-a0606.firebaseapp.com",
+  projectId: "linkstorage-a0606",
+  storageBucket: "linkstorage-a0606.appspot.com",
+  messagingSenderId: "977258947244",
+  appId: "1:977258947244:web:6cb919fbde42e435b93509"
+};
+firebase.initializeApp(firebaseConfig);
