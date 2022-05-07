a9b4c1406bbdde59022f4213acda8372457ec3b1
dayznight7 <dayznight7@gmail.com>
2022-05-07 21:24:34 +0900

    testing storage

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,4 +1,27 @@
 const db = firebase.firestore();
+const storage = firebase.storage();
 //https://gist.github.com/Dohyunwoo/b8370f208619c7f44a2a13fb390e1514
 
+/*
+db.collection("data").doc()
+firebase.auth().onAuthStateChanged((user)=>{
+  if (user) {
+    var uid = user.uid;
+    console.log(uid);
+    db.collection("data").doc(uid).get().then((doc)=>{
 
+      console.log(doc.data());
+      console.log(Object.keys(doc.data()));
+      console.log(Object.values(doc.data()));
+    });
+  }
+  else {
+    location.replace("index.html");
+  }
+});
+*/
+
+storage.ref().child("image/" + "")
+var storageRef = storage.ref();
+var storagePath = storageRef.child(`${uid}/image/`+`001`);
+var storageUpload = storagePath.put(file);
--- js/firebase_config.js
+++ js/firebase_config.js
@@ -16,6 +16,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyD5w7MSZrohow-nd_UeoGZ1bctLI09XNfM",
   authDomain: "linkstorage-a0606.firebaseapp.com",
   projectId: "linkstorage-a0606",
+  databaseURL: "gs://linkstorage-a0606.appspot.com",
   storageBucket: "linkstorage-a0606.appspot.com",
   messagingSenderId: "977258947244",
   appId: "1:977258947244:web:6cb919fbde42e435b93509"
